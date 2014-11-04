d0b726bef132fdecf1ccb45dc6512d7839166840
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-04 22:33:59 +0100

    version 3.0

--- audio/about_wave.py
+++ audio/about_wave.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "audio.wave",
-  __version__     = "3.0-alpha",
+  __version__     = "3.0",
   __license__     = "MIT License",
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = "https://github.com/boisgera/audio.wave",
--- setup.py
+++ setup.py
@@ -3,29 +3,47 @@
 # Python 2.7 Standard Library
 import sys
 
-# Third-Party Libraries
-import setuptools
+# Pip Package Manager
+try:
+    import pip
+    import setuptools
+    import pkg_resources
+except ImportError:
+    error = "pip is not installed, refer to <{url}> for instructions."
+    raise ImportError(error.format(url="http://pip.readthedocs.org"))
 
-# Local Libraries
-sys.path.insert(0, "")
+# Extra Third-Party Libraries
+sys.path.insert(1, ".lib")
+try:
+    setup_requires = ["about>=4.0.0"]
+    require = lambda *r: pkg_resources.WorkingSet().require(*r)
+    require(*setup_requires)
+    import about
+except pkg_resources.DistributionNotFound:
+    error = """{req!r} not found; install it locally with:
+
+    pip install --target=.lib --ignore-installed {req!r}
+"""
+    raise ImportError(error.format(req=" ".join(setup_requires)))
 import about
 
+# Project Metadata 
+sys.path.insert(1, "audio")
+import about_wave
 
 
-metadata = about.get_metadata("about_wave", "audio")
-contents = dict(packages=setuptools.find_packages())
-# TODO: use an external requirements.txt instead and check the existence
-#       of numpy manually ? Or better offer some conditional installation ?
-requirements = dict(install_requires=\
-                    ["numpy", "bitstream", "logfile", "lsprofcalltree"])
-
-# other non-documented dependencies: 'script' (my own, no the one on pypi).
-
-info = {}
-info.update(metadata)
-info.update(contents)
-info.update(requirements)
+info = dict(
+  metadata     = about.get_metadata(about_wave),
+  code         = dict(packages=setuptools.find_packages()),
+  data         = {},
+  requirements = dict(install_requires="numpy bitstream logfile wish".split()),
+  scripts      = {},
+  commands     = {},
+  plugins      = {},
+  tests        = dict(test_suite="test.suite"),
+)
 
 if __name__ == "__main__":
-    setuptools.setup(**info)
+    kwargs = {k:v for dct in info.values() for (k,v) in dct.items()}
+    setuptools.setup(**kwargs)
 
