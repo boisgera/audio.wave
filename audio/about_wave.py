449ba4c281ad2ddd7f001baec65a335593b85d06
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-05 09:44:03 +0100

    version 4.0

--- audio/about_wave.py
+++ audio/about_wave.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "audio.wave",
-  __version__     = "3.0",
+  __version__     = "4.0",
   __license__     = "MIT License",
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = "https://github.com/boisgera/audio.wave",
